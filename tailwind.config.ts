ca6916fb119005b0eb91a233bb6d2a6ded228b67
Adiul Alam Adil <adiulalam@gmail.com>
2023-09-02 21:10:15 +0100

    footer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,27 @@
+import { themeColors } from "./src/utils/themeColors";
 import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        plight: themeColors.primary.light,
+        primary: themeColors.primary.main,
+        pdark: themeColors.primary.dark,
+        slight: themeColors.secondary.light,
+        secondary: themeColors.secondary.main,
+        sdark: themeColors.secondary.dark,
+      },
+    },
+    screens: {
+      xs: "0px",
+      sm: "600px",
+      md: "900px",
+      lg: "1200px",
+      xl: "1536px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 } satisfies Config;
