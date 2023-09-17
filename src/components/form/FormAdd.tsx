f7564c4eefa82b08ce9cf0fcdea4d8bc889f3210
Adiul Alam Adil <adiulalam@gmail.com>
2023-09-17 02:30:17 +0100

    backdrop

--- src/components/form/FormAdd.tsx
+++ src/components/form/FormAdd.tsx
@@ -1,9 +1,12 @@
 import { Add as AddIcon } from "@mui/icons-material";
-import { Fab, Tooltip } from "@mui/material";
+import { Fab, Tooltip, Backdrop, CircularProgress } from "@mui/material";
 import { api } from "@/utils/api";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export const FormAdd = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
   const { push } = useRouter();
 
   const { mutate } = api.form.createForm.useMutation({
@@ -11,18 +14,27 @@ export const FormAdd = () => {
   });
 
   const onClickHandler = () => {
+    setOpen(true);
     mutate({ title: "Untitled" });
   };
 
   return (
-    <Tooltip title="Add Form">
-      <Fab
-        className="fixed bottom-8 right-8 bg-primary"
-        size="large"
-        onClick={onClickHandler}
+    <>
+      <Tooltip title="Add Form">
+        <Fab
+          className="fixed bottom-8 right-8 bg-primary"
+          size="large"
+          onClick={onClickHandler}
+        >
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
       >
-        <AddIcon />
-      </Fab>
-    </Tooltip>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
   );
 };
