0f818e600b60f449cfc5921a0a780e52f79228d1
Adiul Alam Adil <adiulalam@gmail.com>
2023-09-18 00:29:21 +0100

    loading state

--- src/components/form/FormNavbar.tsx
+++ src/components/form/FormNavbar.tsx
@@ -1,15 +1,30 @@
-import { Box } from "@mui/material";
-import { useState } from "react";
+import { Box, Button, CircularProgress } from "@mui/material";
+import { useState, useContext } from "react";
 import { FormTitle } from "./FormTitle";
 import { FormFavourite } from "./FormFavourite";
 import { FormShare } from "./FormShare";
+import { FormContext } from "@/store/FormProvider";
+import { api } from "@/utils/api";
 
-export const FormNavbar = () => {
+export const FormNavbar = ({ isFetching }: { isFetching: boolean }) => {
   const [isReadOnly, setIsReadOnly] = useState<boolean>(true);
+  const { id, status } = useContext(FormContext);
+  const { form } = api.useContext();
+
+  const { mutate } = api.form.updateFormStatus.useMutation({
+    onSuccess: () => form.getPrivateForm.invalidate({ id }),
+  });
+
+  const onClickHandler = () => {
+    mutate({
+      body: { status: status === "DRAFT" ? "COMPLETED" : "DRAFT" },
+      params: { id },
+    });
+  };
 
   return (
     <Box
-      className="flex w-full flex-row items-center justify-between gap-2"
+      className="flex w-full flex-col items-center justify-between gap-2 md:flex-row"
       maxWidth={"xl"}
     >
       <Box className="w-full">
@@ -22,6 +37,15 @@ export const FormNavbar = () => {
       <Box className="flex flex-row items-center justify-between">
         <FormFavourite />
         <FormShare />
+        <Button
+          variant="text"
+          color={status === "DRAFT" ? "error" : "primary"}
+          onClick={onClickHandler}
+        >
+          {status}
+        </Button>
+
+        {isFetching && <CircularProgress color="inherit" size={20} />}
       </Box>
     </Box>
   );
