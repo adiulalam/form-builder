c1aebbcf27c2b575557848e722f8f90ea90e10a6
Adiul Alam Adil <adiulalam@gmail.com>
2023-12-24 20:21:58 +0000

    feat: new add feature

--- __tests__/components/form/FormAdd.test.tsx
+++ __tests__/components/form/FormAdd.test.tsx
@@ -5,8 +5,14 @@ import { AllWithTRPC } from ".";
 describe("Test the 'FormAdd' component", () => {
   it("Should return enabled valid button", () => {
     render(<FormAdd />, { wrapper: AllWithTRPC });
-    expect(screen.getByLabelText("Add Form")).toBeInTheDocument();
+    expect(screen.getByLabelText("Create new form")).toBeInTheDocument();
     expect(screen.getByTestId("AddIcon")).toBeInTheDocument();
+
+    expect(screen.getByLabelText("See Draft forms")).toBeInTheDocument();
+    expect(screen.getByTestId("CreateIcon")).toBeInTheDocument();
+
+    expect(screen.getByLabelText("See Submitted forms")).toBeInTheDocument();
+    expect(screen.getByTestId("CheckIcon")).toBeInTheDocument();
 
     const button = screen.getByRole("button");
     expect(button).not.toBeDisabled();
--- src/components/form/FormAdd.tsx
+++ src/components/form/FormAdd.tsx
@@ -1,8 +1,28 @@
-import { Add as AddIcon } from "@mui/icons-material";
-import { Fab, Tooltip, Backdrop, CircularProgress } from "@mui/material";
+import {
+  Create as CreateIcon,
+  Description as DescriptionIcon,
+  Check as CheckIcon,
+} from "@mui/icons-material";
+import {
+  Backdrop,
+  CircularProgress,
+  SpeedDial,
+  SpeedDialAction,
+  SpeedDialIcon,
+  styled,
+} from "@mui/material";
 import { api } from "@/utils/api";
 import { useRouter } from "next/router";
 import { useState } from "react";
+
+const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
+  ".MuiSpeedDial-fab": {
+    backgroundColor: theme.palette.primary.main,
+  },
+  ".MuiSpeedDial-fab:hover": {
+    backgroundColor: theme.palette.primary.dark,
+  },
+}));
 
 export const FormAdd = () => {
   const [open, setOpen] = useState<boolean>(false);
@@ -18,17 +38,42 @@ export const FormAdd = () => {
     mutate({ title: "Untitled" });
   };
 
+  const actions = [
+    {
+      icon: <CreateIcon />,
+      name: "Create new form",
+      click: onClickHandler,
+    },
+    {
+      icon: <DescriptionIcon />,
+      name: "See Draft forms",
+      click: onClickHandler,
+    },
+    {
+      icon: <CheckIcon />,
+      name: "See Submitted forms",
+      click: onClickHandler,
+    },
+  ];
+
   return (
     <>
-      <Tooltip title="Add Form">
-        <Fab
-          className="fixed bottom-8 right-8 bg-primary"
-          size="large"
-          onClick={onClickHandler}
-        >
-          <AddIcon />
-        </Fab>
-      </Tooltip>
+      <StyledSpeedDial
+        className="fixed bottom-8 right-8"
+        ariaLabel="SpeedDial"
+        icon={<SpeedDialIcon />}
+        aria-disabled={true}
+      >
+        {actions.map((action, index) => (
+          <SpeedDialAction
+            key={index}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            className="bg-sdark"
+            onClick={action.click}
+          />
+        ))}
+      </StyledSpeedDial>
       <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={open}
