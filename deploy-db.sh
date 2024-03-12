99caaa8209ea9b40d9165e39c8f6a57313961c4a
Adiul Alam Adil <adiulalam@gmail.com>
2024-03-12 02:22:36 +0000

    script changes

--- deploy-db.sh
+++ deploy-db.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 source .env
 
-if [ $NODE_ENV == 'production' ]
-then
-    export DATABASE_URL="mysql://$MYSQL_USER:$MYSQL_PASSWORD@db:$MYSQL_PORT/$MYSQL_DATABASE"
-fi
+export DATABASE_URL="mysql://$MYSQL_USER:$MYSQL_PASSWORD@db:$MYSQL_PORT/$MYSQL_DATABASE"
 
-docker-compose up mysql-db -d
 npx prisma migrate deploy
 npx prisma db push
 
-if [ $NODE_ENV == 'production' ]
-then
-    node server.js
-fi
+node server.js
