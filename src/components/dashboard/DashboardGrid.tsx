98708f2a65652bc0255d70af60ac0f76a39cccea
Adiul Alam Adil <adiulalam@gmail.com>
2024-04-19 21:56:10 +0100

    fix: dashboard grid

--- src/components/dashboard/DashboardGrid.tsx
+++ src/components/dashboard/DashboardGrid.tsx
@@ -1,23 +1,29 @@
 import { Box } from "@mui/material";
 import clsx from "clsx";
 import type { ReactNode } from "react";
+import { Children } from "react";
 
 type DashboardGridProps = {
   children: ReactNode;
-  maxCols: number;
+  maxCols: 1 | 2 | 3;
   className?: React.HTMLAttributes<HTMLDivElement>["className"];
 };
+
+const colsMap = {
+  1: "grid-cols-1 h-[20rem]",
+  2: "grid-cols-1 md:grid-cols-2 h-[40rem] md:h-[20rem]",
+  3: "grid-cols-1 md:grid-cols-3 h-[60rem] md:h-[20rem]",
+} as const;
 
 export const DashboardGrid = ({
   children,
   maxCols,
   className,
 }: DashboardGridProps) => {
-  const colsMap = {
-    1: "grid-cols-1 h-[20rem]",
-    2: "grid-cols-1 md:grid-cols-2 h-[40rem] md:h-[20rem]",
-    3: "grid-cols-1 md:grid-cols-3 h-[60rem] md:h-[20rem]",
-  } as Record<number, string>;
+  const count = Children.count(children);
+  if (maxCols !== count) {
+    throw new Error("Number of columns doesn't match number of children");
+  }
 
   return (
     <Box
