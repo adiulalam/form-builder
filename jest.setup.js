d7c18deb26b4b3d8b2e233b449082b4e5986caef
Adiul Alam Adil <adiulalam@gmail.com>
2024-05-04 02:46:10 +0100

    test: mock barchart

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,7 @@
 import "@testing-library/jest-dom";
 jest.mock("next/router", () => require("next-router-mock"));
+
+jest.mock("@mui/x-charts", () => ({
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  BarChart: jest.fn().mockImplementation(({ children }) => children),
+}));
