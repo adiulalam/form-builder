19b18385e084c9b0c7d75bdf16389f53025ac600
Adiul Alam Adil <adiulalam@gmail.com>
2024-06-02 23:23:03 +0100

    build: env changes

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 source .env
 
-echo "Waiting for $MYSQL_HOST to come up..."
-until nc -z 'db' 3306; do
+export MYSQL_HOST="db"
+export DATABASE_URL="mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_HOST:$MYSQL_PORT/$MYSQL_DATABASE"
+
+export PORT=4000
+export NEXTAUTH_URL="http://localhost:$PORT"
+
+echo "Waiting for `$MYSQL_HOST` to come up..."
+until nc -z $MYSQL_HOST 3306; do
     echo "Waiting for database connection..."
     sleep 1
 done
