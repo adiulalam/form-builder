2667545928a35a1fd9e0caef0d4a9801b5f841be
Adiul Alam Adil <adiulalam@gmail.com>
2024-06-02 23:23:18 +0100

    test: seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,87 @@
-import { PrismaClient } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
+import { PrismaClient, Status, Type } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+import _ from "lodash";
+
+type CreateFormType = Prisma.FormUncheckedCreateInput[];
+type CreateQuestionsType = Prisma.QuestionCreateManyInput[];
+type CreateOptionsType = Prisma.OptionCreateManyInput[];
+
 const prisma = new PrismaClient();
 
-async function main() {
-  const userId = await prisma.user.findFirst({
+const createOptions = (
+  questionId: string,
+  showInput: boolean,
+  count: number
+): CreateOptionsType => {
+  const options: CreateOptionsType = _.range(count).map(() => ({
+    questionId,
+    showInput,
+    value: faker.word.sample(),
+  }));
+
+  return options;
+};
+
+const createQuestions = (
+  formId: string,
+  count: number
+): CreateQuestionsType => {
+  const questions: CreateQuestionsType = _.range(count).map((index) => ({
+    formId,
+    question: faker.lorem.sentence(6),
+    order: index,
+    type: faker.helpers.enumValue(Type),
+  }));
+
+  return questions;
+};
+
+const createForms = (userId: string, count: number): CreateFormType => {
+  const forms: CreateFormType = _.range(count).map(() => ({
+    title: faker.lorem.sentence(3),
+    status: faker.helpers.enumValue(Status),
+    userId,
+  }));
+
+  return forms;
+};
+
+const main = async () => {
+  const user = await prisma.user.findFirst({
     orderBy: { createdAt: "desc" },
   });
 
-  if (!userId) return new Error("No user id found");
+  if (!user) return new Error("No user id found");
 
-  const formId = "1fc7e619-78c1-4e81-aa4b-07e8916a33fa";
+  // Create 5 forms
+  const forms = await prisma.$transaction(
+    createForms(user.id, 5).map((form) => prisma.form.create({ data: form }))
+  );
+  console.log("Forms Created");
 
-  const testForm = await prisma.form.upsert({
-    where: { id: formId },
-    update: {},
-    create: {
-      id: formId,
-      userId: userId.id,
-      title: "test title",
-      status: "DRAFT",
-    },
-  });
-  console.log({ userId, testForm });
-}
+  // Each forms has 5 questions
+  const formsQuestions = forms.flatMap((form) =>
+    createQuestions(form.id, 5).map((question) =>
+      prisma.question.create({ data: question })
+    )
+  );
+  const questions = await prisma.$transaction(formsQuestions);
+  console.log("Questions Created");
+
+  // Each questions has 5 options, except type input
+  const questionsOptions = questions.flatMap((question) =>
+    createOptions(
+      question.id,
+      question.type === "INPUT",
+      question.type === "INPUT" ? 1 : 5
+    ).map((option) => prisma.option.create({ data: option }))
+  );
+
+  const options = await prisma.$transaction(questionsOptions);
+  console.log("Options Created", options);
+};
+
 main()
   .then(async () => {
     await prisma.$disconnect();
