abcec60c893ad4404aa94be7e44bfdc24e1bdb05
Adiul Alam Adil <adiulalam@gmail.com>
2024-06-08 23:26:55 +0100

    build: conditinal build env

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-source .env
+
+if [ -f .env ]; then
+    echo "Loading environment variables from .env file"
+    source .env
+else
+    echo ".env file not found, skipping"
+fi
+
+export MYSQL_PORT="3306"
+echo "Exporting MYSQL_PORT to $MYSQL_PORT"
 
 export MYSQL_HOST="db"
+echo "Exporting MYSQL_HOST to $MYSQL_HOST"
+
 export DATABASE_URL="mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_HOST:$MYSQL_PORT/$MYSQL_DATABASE"
+echo "Exporting DATABASE_URL to $DATABASE_URL"
 
 export PORT=$PORT
+echo "Exporting PORT to $PORT"
 
 echo "Waiting for `$MYSQL_HOST` to come up..."
 until nc -z $MYSQL_HOST 3306; do
